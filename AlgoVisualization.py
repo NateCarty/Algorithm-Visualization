e634ec8e4d518a36fea77e2ed5036c08e28bc29c
Nate Carty <natecartylife@gmail.com>
2021-12-13 16:06:19 -0500

    Started tkinter page for visualization

--- AlgoVisualization.py
+++ AlgoVisualization.py
@@ -4,7 +4,25 @@ import matplotlib
 matplotlib.use("TkAgg")
 
 # import the figure canvas for tkagg and a navigation bar
-from matplotlib.backends.backend_tkagg import FigureCanvasAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg, NavigationToolbar2Tk
 
 # import regular Figure
 from matplotlib.figure import Figure
+
+import tkinter as tk
+from tkinter import ttk
+
+class graphPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        
+        # inititialize frame
+        tk.Frame.__init__(self, parent)
+
+        # create label for current algorithm
+        exampleAlgo = "Insertion"
+        label = tk.Label(self, text= f"{exampleAlgo} Algorithm!")
+        label.pack(pady=10, padx=10)
+
+        # insert live matplotlib graph here
+
