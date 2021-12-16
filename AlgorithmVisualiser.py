6e676161c5cf7bca0ebddaba2dad641a56c4692d
Nate Carty <natecartylife@gmail.com>
2021-12-16 00:14:51 -0500

    Added drop-down menus for color and algorithm

--- AlgorithmVisualiser.py
+++ AlgorithmVisualiser.py
@@ -1,8 +1,38 @@
 import tkinter as tk
+from tkinter import OptionMenu
+from tkinter import StringVar
+
+class Window(tk.Frame):
+    def __init__(self, master = None, **kwargs):
+        tk.Frame.__init__(self, master, **kwargs)
+
+        buttonFrame = tk.Frame(self)
+        buttonFrame.pack()
+
+        self.colorSelectorVar = StringVar()
+        self.colorSelectorVar.set("Green")
+        self.colorSelector = OptionMenu(buttonFrame, self.colorSelectorVar, "Green", "Yellow", "Purple", "Orange", "Blue", "Red")
+        self.colorSelector.pack(side=tk.LEFT)
+        self.colorSelector.config(bg="#add8e6", activebackground="#6fbbd3", highlightbackground="white")
+
+        self.currentAlgo = StringVar()
+        self.currentAlgo.set("Insertion")
+        self.selector = OptionMenu(buttonFrame, self.currentAlgo, "Insertion", "Selection", "Bubble", "Quick Sort", "Heap")
+        self.selector.pack(side=tk.LEFT)
+        self.selector.config(bg="#add8e6", activebackground="#6fbbd3", highlightbackground="white")
+
+
 
 def main():
+
+    # create tkinter interface
     root = tk.Tk()
     root.title("Sorting Algorithm Visualiser")
+
+    # create main window
+    window = Window(root)
+    window.pack()
+
     root.mainloop()
 
 if __name__ == "__main__":
