a73f5fd80882ff277c3adb29f2c5f841af55da16
Nate Carty <natecartylife@gmail.com>
2021-12-16 16:18:11 -0500

    Added Heap sort functionality

--- AlgorithmVisualiser.py
+++ AlgorithmVisualiser.py
@@ -211,7 +211,36 @@ class Window(tk.Frame):
                     quick_sort(p + 1, end, array)
             
             quick_sort(0, len(self.intArray) - 1, self.intArray)
-            
+
+        elif self.currentAlgorithm == "Heap":
+            def heapify(arr, n, i):
+                largest = i
+                l = 2 * i + 1
+                r = 2 * i + 2
+
+                if l < n and arr[largest] < arr[l]:
+                    largest = l
+
+                if r < n and arr[largest] < arr[r]:
+                    largest = r
+
+                if largest != i:
+                    arr[i], arr[largest] = arr[largest], arr[i]  
+
+                    heapify(arr, n, largest)
+
+            def heapSort(arr):
+                n = len(arr)
+
+                for i in range(n//2 - 1, -1, -1):
+                    heapify(arr, n, i)
+
+                for i in range(n-1, 0, -1):
+                    arr[i], arr[0] = arr[0], arr[i]  
+                    heapify(arr, i, 0)
+
+            heapSort(self.intArray)
+
     # function when start button is pressed
     def on_start(self):
 
